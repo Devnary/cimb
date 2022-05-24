d3e32e139a4264b2547541b187c9fd43a41a133e
Devnary <81035912+Devnary@users.noreply.github.com>
2022-05-24 19:59:12 +0200

    Update img2ppm.py

--- img2ppm.py
+++ img2ppm.py
@@ -3,4 +3,6 @@ from PIL import Image
 
 if __name__ == "__main__":
     im = Image.open(sys.argv[1])
+    width, height = im.size
+    im = im.resize((round(width/20)*20,round(height/40)*40))
     im.save(sys.argv[1].split(".")[0]+".ppm") 
